25e92e8e8edefdc937bcd615fb53d7bf1687f16d
shin1 <gci787@naver.com>
2023-03-05 11:50:41 +0900

    build폴더추가

--- src/components/Add.js
+++ src/components/Add.js
@@ -7,15 +7,18 @@ export default function Add(){
     axios.defaults.withCredentials = true;
     const url = 'https://openapi.naver.com/v1/papago/n2mt';
     const [data, setData] = useState([]);
+    console.log(process.env.REACT_APP_PAPAGO_ID);
+    console.log(process.env.REACT_APP_PAPAGO_PSW);
     const onTrans= async () =>{
+
         try{
             axios.post(url, {
                 form: {'source':'ko', 'target':'en', 'text':'test'},
                 headers : {
                     'Access-Control-Allow-Origin': "*",
                     'Content-Type': 'application/xml; charset=utf-8',
-                    'X-Naver-Client-Id' : '',
-                    'X-Naver-Client-Secret': ''
+                    'X-Naver-Client-Id' : process.env.REACT_APP_ID,
+                    'X-Naver-Client-Secret': process.env.REACT_APP_PAPAGO_PSW 
                 },
                 withCredentials : true,
                 credentials: 'same-origin',
